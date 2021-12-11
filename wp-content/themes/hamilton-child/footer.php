ffb2a4f207266afd64201ea30d5bcec9d59fab96
Julie Eggertsen <juli741j@stud.kea.dk>
2021-12-11 21:20:46 +0100

    footer

---
+++ wp-content/themes/hamilton-child/footer.php
@@ -0,0 +1,32 @@
+</main>
+		
+		<footer class="site-footer section-inner">
+
+        <div>        
+        <ul>
+         <li>Privatlivspolitik <a href="#privatlivspolitik"></a></li>
+         <li>Virksomhedsinformationer <a href="#virksomhedsinformationer"></a></li>
+         <li>Betaling- og retur <a href="#betaling"></a></li>
+          </ul>
+          </div>
+                                
+     <div>           
+     <a href="#privatlivspolitik"> Handelsbetingelser</a>
+
+
+     <li>Servicevilkår <a href="#privatlivspolitik"></a></li>
+     <li>Cookiepolitik <a href="#privatlivspolitik"></a></li>
+       </ul>
+      </div>
+                                 
+      <div>   
+      <a href="#insta"><img src="" alt="inst"></a>      
+
+          </div>
+
+		</footer>
+	    
+	    <?php wp_footer(); ?>
+	        
+	</body>
+</html>
