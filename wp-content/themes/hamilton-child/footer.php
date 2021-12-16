446c3f9f87dc3a043588e5fc9161f097a0a4e76d
“valgfrit-men-brug-bare-dit-GitHub-brugernavn” <din-GitHub-email@stud.kea.dk>
2021-12-16 23:54:08 +0100

    footer ny

--- wp-content/themes/hamilton-child/footer.php
+++ wp-content/themes/hamilton-child/footer.php
@@ -5,20 +5,20 @@
 <div class="row">
     
 <div class="column">
-<a href=""><p>Politikker</p></a>
+<a href="https://kirstinekrogs.dk/kea/Dinplanteven/privacy-policy/"><p>Politikker</p></a>
 <a href=""><p>Virksomhedsinformationer</p></a>
-<a href=""><p>Betaling- og retur</p></a>
+<a href="https://kirstinekrogs.dk/kea/Dinplanteven/?page_id=177"><p>Betaling- og retur</p></a>
 </div>
 
 <div class="column">
 <a href=""><p>Handelsbetingelser</p></a>
 <a href=""><p>Servicevilkår</p></a>
-<a href=""><p>Cookiepolitik</p></a>
+<a href="https://kirstinekrogs.dk/kea/Dinplanteven/cookie-policy-eu/"><p>Cookiepolitik</p></a>
 </div>
 
 <div class="social">
-<a href="#fb" class="facebook"><img src="https://kirstinekrogs.dk/kea/Dinplanteven/wp-content/uploads/2021/12/fb.svg" alt="fb ikon"></a>
-<a href="#Insta" class="instagram"><img src="https://kirstinekrogs.dk/kea/Dinplanteven/wp-content/uploads/2021/12/Instagram.svg" alt="insta ikon"></a>
+<a href="https://www.facebook.com/Dinplantevenmmddk-106563078469156" class="facebook"><img src="https://kirstinekrogs.dk/kea/Dinplanteven/wp-content/uploads/2021/12/fb.svg" alt="fb ikon"></a>
+<a href="https://www.instagram.com/dinplantevenmmd.dk/" class="instagram"><img src="https://kirstinekrogs.dk/kea/Dinplanteven/wp-content/uploads/2021/12/Instagram.svg" alt="insta ikon"></a>
 </div>
 
 <div class="flower">
